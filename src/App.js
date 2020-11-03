166c929e4b70f3c074d7e033633c52e0f51384a7
Anvay Mandar Joshi <anvayj@cybage.com>
2020-11-03 15:59:15 +0530

    Reset to default App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Sixteen Clothing Website Testing !!!</h1>
-          <h1>Wow !!!</h1>
+          <h1>Sixteen Clothing Website</h1>
       </div>
     );
   }
