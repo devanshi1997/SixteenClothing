be08432ae625994260b9c00b3845dc72c65b7851
Anvay Mandar Joshi <anvayj@cybage.com>
2020-12-02 09:36:42 +0530

    Configure basic routing of pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Home from './Pages/Home';
 import OurProducts from './Pages/OurProducts';
 import AboutUs from './Pages/AboutUs';
 import ContactUS from './Pages/ContactUs';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1>Sixteen Clothing Website</h1>
-          <Home />
-          <OurProducts />
-          <AboutUs />
-          <ContactUS />
-      </div>
+      <Router>
+        <Navigation />
+            <Redirect to="/home" />
+            <Route path="/home" component={Home} />
+            <Route path="/products" component={OurProducts} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/contact" component={ContactUS} />
+      </Router>
     );
   }
 }
