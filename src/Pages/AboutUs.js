fd73a04ce1c605e564bc34c97a6594c782832aca
Shubhraneel G <shubhraneelg@cybage.com>
2020-12-04 11:21:07 +0530

    componet Our Team

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
 import Banner from '../components/Banner/Banner'
+import OurTeam from '../components/OurTeam/OurTeam';
 
 const AboutUs = ()=>{
     return (
         <>
         <Banner type="simple" category="About Us Banner" />
-           
+           <OurTeam />
             </>
 
     );
