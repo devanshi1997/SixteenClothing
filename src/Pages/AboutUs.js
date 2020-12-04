1c15fb7b87167e3465fbef7c347fe9962b07e0f4
Helee Dhiraj Parekh <heleep@cybage.com>
2020-12-04 11:56:25 +0530

    Happy partners About Us

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner'
 import ServicesSixteenClothing from "../components/ServicesSixteenClothing/ServicesSixteenClothing";
+import HappyPartners from "../components/HappyPartners/HappyPartners"
 
 const AboutUs = ()=>{
     return (
         <>
             <Banner type="simple" category="About Us Banner" />
-                <ServicesSixteenClothing />
+            <ServicesSixteenClothing />
+            <HappyPartners/>
         </>
 
     );
